d886a42bddb2a37ec298c6b2bda9b8d90e844744
Jorden Chamid <jchamid@totallyhosted.nl>
2016-06-01 14:38:28 +0200

    All (known) keys added

--- drivers/smarttv/driver.js
+++ drivers/smarttv/driver.js
@@ -6,166 +6,449 @@ var http = require('http');
 var net = require('net');
 
 var allpossibleKeys = [
-		{	inputName: '26',
-	 		friendlyName: "KEY_IDX_MUTE"
+		{	inputName: "400",
+		friendlyName: "KEY_IDX_3D"
 		},
-		{	inputName: '24',
-	 		friendlyName: "KEY_IDX_VOL_UP"
+		{	inputName: "13",
+		friendlyName: "KEY_IDX_ARROW_DOWN"
 		},
-		{	inputName: '25',
-	 		friendlyName: "KEY_IDX_VOL_DOWN"
+		{	inputName: "14",
+		friendlyName: "KEY_IDX_ARROW_LEFT"
+		},
+		{	inputName: "15",
+		friendlyName: "KEY_IDX_ARROW_RIGHT"
+		},
+		{	inputName: "12",
+		friendlyName: "KEY_IDX_ARROW_UP"
+		},
+		{	inputName: "23",
+		friendlyName: "KEY_IDX_BACK"
+		},
+		{	inputName: "29",
+		friendlyName: "KEY_IDX_BLUE"
+		},
+		{	inputName: "28",
+		friendlyName: "KEY_IDX_CH_DOWN"
+		},
+		{	inputName: "27",
+		friendlyName: "KEY_IDX_CH_UP"
+		},
+		{	inputName: "472",
+		friendlyName: "KEY_IDX_DATA"
+		},
+		{	inputName: "20",
+		friendlyName: "KEY_IDX_ENTER"
+		},
+		{	inputName: "412",
+		friendlyName: "KEY_IDX_EXIT"
+		},
+		{	inputName: "47",
+		friendlyName: "KEY_IDX_EXTERNAL_INPUT"
+		},
+		{	inputName: "36",
+		friendlyName: "KEY_IDX_FORWARD"
+		},
+		{	inputName: "2",
+		friendlyName: "KEY_IDX_GAMEPAD_ARROW_DOWN"
+		},
+		{	inputName: "3",
+		friendlyName: "KEY_IDX_GAMEPAD_ARROW_LEFT"
+		},
+		{	inputName: "4",
+		friendlyName: "KEY_IDX_GAMEPAD_ARROW_RIGHT"
+		},
+		{	inputName: "1",
+		friendlyName: "KEY_IDX_GAMEPAD_ARROW_UP"
+		},
+		{	inputName: "5",
+		friendlyName: "KEY_IDX_GAMEPAD_BTN_1"
+		},
+		{	inputName: "6",
+		friendlyName: "KEY_IDX_GAMEPAD_BTN_2"
+		},
+		{	inputName: "7",
+		friendlyName: "KEY_IDX_GAMEPAD_BTN_3"
+		},
+		{	inputName: "8",
+		friendlyName: "KEY_IDX_GAMEPAD_BTN_4"
+		},
+		{	inputName: "9",
+		friendlyName: "KEY_IDX_GAMEPAD_MENU"
+		},
+		{	inputName: "30",
+		friendlyName: "KEY_IDX_GREEN"
+		},
+		{	inputName: "21",
+		friendlyName: "KEY_IDX_HOME"
+		},
+		{	inputName: "419",
+		friendlyName: "KEY_IDX_JP_BS"
+		},
+		{	inputName: "420",
+		friendlyName: "KEY_IDX_JP_BS_NUM_1"
+		},
+		{	inputName: "429",
+		friendlyName: "KEY_IDX_JP_BS_NUM_10"
+		},
+		{	inputName: "430",
+		friendlyName: "KEY_IDX_JP_BS_NUM_11"
+		},
+		{	inputName: "431",
+		friendlyName: "KEY_IDX_JP_BS_NUM_12"
+		},
+		{	inputName: "421",
+		friendlyName: "KEY_IDX_JP_BS_NUM_2"
+		},
+		{	inputName: "422",
+		friendlyName: "KEY_IDX_JP_BS_NUM_3"
+		},
+		{	inputName: "423",
+		friendlyName: "KEY_IDX_JP_BS_NUM_4"
+		},
+		{	inputName: "424",
+		friendlyName: "KEY_IDX_JP_BS_NUM_5"
+		},
+		{	inputName: "425",
+		friendlyName: "KEY_IDX_JP_BS_NUM_6"
+		},
+		{	inputName: "426",
+		friendlyName: "KEY_IDX_JP_BS_NUM_7"
+		},
+		{	inputName: "427",
+		friendlyName: "KEY_IDX_JP_BS_NUM_8"
+		},
+		{	inputName: "428",
+		friendlyName: "KEY_IDX_JP_BS_NUM_9"
+		},
+		{	inputName: "432",
+		friendlyName: "KEY_IDX_JP_CS1"
+		},
+		{	inputName: "433",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_1"
+		},
+		{	inputName: "442",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_10"
+		},
+		{	inputName: "443",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_11"
+		},
+		{	inputName: "444",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_12"
+		},
+		{	inputName: "434",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_2"
+		},
+		{	inputName: "435",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_3"
+		},
+		{	inputName: "436",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_4"
+		},
+		{	inputName: "437",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_5"
+		},
+		{	inputName: "438",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_6"
+		},
+		{	inputName: "439",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_7"
+		},
+		{	inputName: "440",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_8"
+		},
+		{	inputName: "441",
+		friendlyName: "KEY_IDX_JP_CS1_NUM_9"
+		},
+		{	inputName: "445",
+		friendlyName: "KEY_IDX_JP_CS2"
+		},
+		{	inputName: "446",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_1"
+		},
+		{	inputName: "455",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_10"
+		},
+		{	inputName: "456",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_11"
+		},
+		{	inputName: "457",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_12"
+		},
+		{	inputName: "447",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_2"
+		},
+		{	inputName: "448",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_3"
+		},
+		{	inputName: "449",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_4"
+		},
+		{	inputName: "450",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_5"
+		},
+		{	inputName: "451",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_6"
+		},
+		{	inputName: "452",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_7"
+		},
+		{	inputName: "453",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_8"
+		},
+		{	inputName: "454",
+		friendlyName: "KEY_IDX_JP_CS2_NUM_9"
+		},
+		{	inputName: "458",
+		friendlyName: "KEY_IDX_JP_JS"
+		},
+		{	inputName: "459",
+		friendlyName: "KEY_IDX_JP_JS_NUM_1"
+		},
+		{	inputName: "468",
+		friendlyName: "KEY_IDX_JP_JS_NUM_10"
+		},
+		{	inputName: "469",
+		friendlyName: "KEY_IDX_JP_JS_NUM_11"
+		},
+		{	inputName: "470",
+		friendlyName: "KEY_IDX_JP_JS_NUM_12"
+		},
+		{	inputName: "460",
+		friendlyName: "KEY_IDX_JP_JS_NUM_2"
+		},
+		{	inputName: "461",
+		friendlyName: "KEY_IDX_JP_JS_NUM_3"
+		},
+		{	inputName: "462",
+		friendlyName: "KEY_IDX_JP_JS_NUM_4"
+		},
+		{	inputName: "463",
+		friendlyName: "KEY_IDX_JP_JS_NUM_5"
+		},
+		{	inputName: "464",
+		friendlyName: "KEY_IDX_JP_JS_NUM_6"
+		},
+		{	inputName: "465",
+		friendlyName: "KEY_IDX_JP_JS_NUM_7"
+		},
+		{	inputName: "466",
+		friendlyName: "KEY_IDX_JP_JS_NUM_8"
+		},
+		{	inputName: "467",
+		friendlyName: "KEY_IDX_JP_JS_NUM_9"
+		},
+		{	inputName: "22",
+		friendlyName: "KEY_IDX_MENU"
+		},
+		{	inputName: "26",
+		friendlyName: "KEY_IDX_MUTE"
+		},
+		{	inputName: "417",
+		friendlyName: "KEY_IDX_MYAPPS"
+		},
+		{	inputName: "408",
+		friendlyName: "KEY_IDX_NETCAST"
+		},
+		{	inputName: "2",
+		friendlyName: "KEY_IDX_NUM_0"
+		},
+		{	inputName: "3",
+		friendlyName: "KEY_IDX_NUM_1"
+		},
+		{	inputName: "4",
+		friendlyName: "KEY_IDX_NUM_2"
+		},
+		{	inputName: "5",
+		friendlyName: "KEY_IDX_NUM_3"
+		},
+		{	inputName: "6",
+		friendlyName: "KEY_IDX_NUM_4"
+		},
+		{	inputName: "7",
+		friendlyName: "KEY_IDX_NUM_5"
+		},
+		{	inputName: "8",
+		friendlyName: "KEY_IDX_NUM_6"
+		},
+		{	inputName: "9",
+		friendlyName: "KEY_IDX_NUM_7"
+		},
+		{	inputName: "10",
+		friendlyName: "KEY_IDX_NUM_8"
+		},
+		{	inputName: "11",
+		friendlyName: "KEY_IDX_NUM_9"
+		},
+		{	inputName: "34",
+		friendlyName: "KEY_IDX_PAUSE"
+		},
+		{	inputName: "33",
+		friendlyName: "KEY_IDX_PLAY"
+		},
+		{	inputName: "1",
+		friendlyName: "KEY_IDX_POWER_OFF"
+		},
+		{	inputName: "403",
+		friendlyName: "KEY_IDX_PREV_CHANNEL"
+		},
+		{	inputName: "405",
+		friendlyName: "KEY_IDX_QMENU"
+		},
+		{	inputName: "40",
+		friendlyName: "KEY_IDX_REC"
+		},
+		{	inputName: "31",
+		friendlyName: "KEY_IDX_RED"
+		},
+		{	inputName: "37",
+		friendlyName: "KEY_IDX_REWIND"
+		},
+		{	inputName: "1",
+		friendlyName: "KEY_IDX_SIMPLELINK_DISC_MENU"
+		},
+		{	inputName: "4",
+		friendlyName: "KEY_IDX_SIMPLELINK_GUIDE"
+		},
+		{	inputName: "0",
+		friendlyName: "KEY_IDX_SIMPLELINK_HOME"
+		},
+		{	inputName: "2",
+		friendlyName: "KEY_IDX_SIMPLELINK_INFO_MENU"
+		},
+		{	inputName: "6",
+		friendlyName: "KEY_IDX_SIMPLELINK_POWER_ONOFF"
+		},
+		{	inputName: "5",
+		friendlyName: "KEY_IDX_SIMPLELINK_RECLIST"
+		},
+		{	inputName: "3",
+		friendlyName: "KEY_IDX_SIMPLELINK_TITLE_POPUP"
+		},
+		{	inputName: "39",
+		friendlyName: "KEY_IDX_SKIP_BACKWARD"
+		},
+		{	inputName: "38",
+		friendlyName: "KEY_IDX_SKIP_FORWARD"
+		},
+		{	inputName: "30",
+		friendlyName: "KEY_IDX_STB_BACK"
+		},
+		{	inputName: "16",
+		friendlyName: "KEY_IDX_STB_BLUE"
+		},
+		{	inputName: "38",
+		friendlyName: "KEY_IDX_STB_CH_DOWN"
+		},
+		{	inputName: "37",
+		friendlyName: "KEY_IDX_STB_CH_UP"
+		},
+		{	inputName: "19",
+		friendlyName: "KEY_IDX_STB_DASH"
+		},
+		{	inputName: "34",
+		friendlyName: "KEY_IDX_STB_DOWN"
+		},
+		{	inputName: "7",
+		friendlyName: "KEY_IDX_STB_EXIT"
+		},
+		{	inputName: "12",
+		friendlyName: "KEY_IDX_STB_FORWARD"
+		},
+		{	inputName: "14",
+		friendlyName: "KEY_IDX_STB_GREEN"
+		},
+		{	inputName: "2",
+		friendlyName: "KEY_IDX_STB_GUIDE"
+		},
+		{	inputName: "35",
+		friendlyName: "KEY_IDX_STB_LEFT"
+		},
+		{	inputName: "1",
+		friendlyName: "KEY_IDX_STB_MENU"
+		},
+		{	inputName: "29",
+		friendlyName: "KEY_IDX_STB_NUM0"
+		},
+		{	inputName: "20",
+		friendlyName: "KEY_IDX_STB_NUM1"
+		},
+		{	inputName: "21",
+		friendlyName: "KEY_IDX_STB_NUM2"
+		},
+		{	inputName: "22",
+		friendlyName: "KEY_IDX_STB_NUM3"
+		},
+		{	inputName: "23",
+		friendlyName: "KEY_IDX_STB_NUM4"
+		},
+		{	inputName: "24",
+		friendlyName: "KEY_IDX_STB_NUM5"
+		},
+		{	inputName: "25",
+		friendlyName: "KEY_IDX_STB_NUM6"
+		},
+		{	inputName: "26",
+		friendlyName: "KEY_IDX_STB_NUM7"
+		},
+		{	inputName: "27",
+		friendlyName: "KEY_IDX_STB_NUM8"
+		},
+		{	inputName: "28",
+		friendlyName: "KEY_IDX_STB_NUM9"
+		},
+		{	inputName: "18",
+		friendlyName: "KEY_IDX_STB_OK"
+		},
+		{	inputName: "11",
+		friendlyName: "KEY_IDX_STB_PAUSE"
+		},
+		{	inputName: "10",
+		friendlyName: "KEY_IDX_STB_PLAY"
+		},
+		{	inputName: "0",
+		friendlyName: "KEY_IDX_STB_POWER_ONOFF"
+		},
+		{	inputName: "17",
+		friendlyName: "KEY_IDX_STB_REC"
+		},
+		{	inputName: "13",
+		friendlyName: "KEY_IDX_STB_RED"
+		},
+		{	inputName: "8",
+		friendlyName: "KEY_IDX_STB_REWIND"
+		},
+		{	inputName: "36",
+		friendlyName: "KEY_IDX_STB_RIGHT"
+		},
+		{	inputName: "61",
+		friendlyName: "KEY_IDX_STB_SKIP_BACKWARD"
+		},
+		{	inputName: "62",
+		friendlyName: "KEY_IDX_STB_SKIP_FORWARD"
+		},
+		{	inputName: "9",
+		friendlyName: "KEY_IDX_STB_STOP"
+		},
+		{	inputName: "33",
+		friendlyName: "KEY_IDX_STB_UP"
+		},
+		{	inputName: "15",
+		friendlyName: "KEY_IDX_STB_YELLOW"
+		},
+		{	inputName: "35",
+		friendlyName: "KEY_IDX_STOP"
+		},
+		{	inputName: "25",
+		friendlyName: "KEY_IDX_VOL_DOWN"
+		},
+		{	inputName: "24",
+		friendlyName: "KEY_IDX_VOL_UP"
+		},
+		{	inputName: "32",
+		friendlyName: "KEY_IDX_YELLOW"
 		}
 ];
 
-/*
-KEY_IDX_3D                      characterKey=400
-KEY_IDX_ARROW_DOWN              characterKey=13
-KEY_IDX_ARROW_LEFT              characterKey=14
-KEY_IDX_ARROW_RIGHT             characterKey=15
-KEY_IDX_ARROW_UP                characterKey=12
-KEY_IDX_BACK                    characterKey=23
-KEY_IDX_BLUE                    characterKey=29
-KEY_IDX_CH_DOWN                 characterKey=28
-KEY_IDX_CH_UP                   characterKey=27
-KEY_IDX_DATA                    characterKey=472
-KEY_IDX_ENTER                   characterKey=20
-KEY_IDX_EXIT                    characterKey=412
-KEY_IDX_EXTERNAL_INPUT          characterKey=47
-KEY_IDX_FORWARD                 characterKey=36
-KEY_IDX_GAMEPAD_ARROW_DOWN      characterKey=2
-KEY_IDX_GAMEPAD_ARROW_LEFT      characterKey=3
-KEY_IDX_GAMEPAD_ARROW_RIGHT     characterKey=4
-KEY_IDX_GAMEPAD_ARROW_UP        characterKey=1
-KEY_IDX_GAMEPAD_BTN_1           characterKey=5
-KEY_IDX_GAMEPAD_BTN_2           characterKey=6
-KEY_IDX_GAMEPAD_BTN_3           characterKey=7
-KEY_IDX_GAMEPAD_BTN_4           characterKey=8
-KEY_IDX_GAMEPAD_MENU            characterKey=9
-KEY_IDX_GREEN                   characterKey=30
-KEY_IDX_HOME                    characterKey=21
-KEY_IDX_JP_BS                   characterKey=419
-KEY_IDX_JP_BS_NUM_1             characterKey=420
-KEY_IDX_JP_BS_NUM_10            characterKey=429
-KEY_IDX_JP_BS_NUM_11            characterKey=430
-KEY_IDX_JP_BS_NUM_12            characterKey=431
-KEY_IDX_JP_BS_NUM_2             characterKey=421
-KEY_IDX_JP_BS_NUM_3             characterKey=422
-KEY_IDX_JP_BS_NUM_4             characterKey=423
-KEY_IDX_JP_BS_NUM_5             characterKey=424
-KEY_IDX_JP_BS_NUM_6             characterKey=425
-KEY_IDX_JP_BS_NUM_7             characterKey=426
-KEY_IDX_JP_BS_NUM_8             characterKey=427
-KEY_IDX_JP_BS_NUM_9             characterKey=428
-KEY_IDX_JP_CS1                  characterKey=432
-KEY_IDX_JP_CS1_NUM_1            characterKey=433
-KEY_IDX_JP_CS1_NUM_10           characterKey=442
-KEY_IDX_JP_CS1_NUM_11           characterKey=443
-KEY_IDX_JP_CS1_NUM_12           characterKey=444
-KEY_IDX_JP_CS1_NUM_2            characterKey=434
-KEY_IDX_JP_CS1_NUM_3            characterKey=435
-KEY_IDX_JP_CS1_NUM_4            characterKey=436
-KEY_IDX_JP_CS1_NUM_5            characterKey=437
-KEY_IDX_JP_CS1_NUM_6            characterKey=438
-KEY_IDX_JP_CS1_NUM_7            characterKey=439
-KEY_IDX_JP_CS1_NUM_8            characterKey=440
-KEY_IDX_JP_CS1_NUM_9            characterKey=441
-KEY_IDX_JP_CS2                  characterKey=445
-KEY_IDX_JP_CS2_NUM_1            characterKey=446
-KEY_IDX_JP_CS2_NUM_10           characterKey=455
-KEY_IDX_JP_CS2_NUM_11           characterKey=456
-KEY_IDX_JP_CS2_NUM_12           characterKey=457
-KEY_IDX_JP_CS2_NUM_2            characterKey=447
-KEY_IDX_JP_CS2_NUM_3            characterKey=448
-KEY_IDX_JP_CS2_NUM_4            characterKey=449
-KEY_IDX_JP_CS2_NUM_5            characterKey=450
-KEY_IDX_JP_CS2_NUM_6            characterKey=451
-KEY_IDX_JP_CS2_NUM_7            characterKey=452
-KEY_IDX_JP_CS2_NUM_8            characterKey=453
-KEY_IDX_JP_CS2_NUM_9            characterKey=454
-KEY_IDX_JP_JS                   characterKey=458
-KEY_IDX_JP_JS_NUM_1             characterKey=459
-KEY_IDX_JP_JS_NUM_10            characterKey=468
-KEY_IDX_JP_JS_NUM_11            characterKey=469
-KEY_IDX_JP_JS_NUM_12            characterKey=470
-KEY_IDX_JP_JS_NUM_2             characterKey=460
-KEY_IDX_JP_JS_NUM_3             characterKey=461
-KEY_IDX_JP_JS_NUM_4             characterKey=462
-KEY_IDX_JP_JS_NUM_5             characterKey=463
-KEY_IDX_JP_JS_NUM_6             characterKey=464
-KEY_IDX_JP_JS_NUM_7             characterKey=465
-KEY_IDX_JP_JS_NUM_8             characterKey=466
-KEY_IDX_JP_JS_NUM_9             characterKey=467
-KEY_IDX_MENU                    characterKey=22
-KEY_IDX_MUTE                    characterKey=26
-KEY_IDX_MYAPPS                  characterKey=417
-KEY_IDX_NETCAST                 characterKey=408
-KEY_IDX_NUM_0                   characterKey=2
-KEY_IDX_NUM_1                   characterKey=3
-KEY_IDX_NUM_2                   characterKey=4
-KEY_IDX_NUM_3                   characterKey=5
-KEY_IDX_NUM_4                   characterKey=6
-KEY_IDX_NUM_5                   characterKey=7
-KEY_IDX_NUM_6                   characterKey=8
-KEY_IDX_NUM_7                   characterKey=9
-KEY_IDX_NUM_8                   characterKey=10
-KEY_IDX_NUM_9                   characterKey=11
-KEY_IDX_PAUSE                   characterKey=34
-KEY_IDX_PLAY                    characterKey=33
-KEY_IDX_POWER_OFF               characterKey=1
-KEY_IDX_PREV_CHANNEL            characterKey=403
-KEY_IDX_QMENU                   characterKey=405
-KEY_IDX_REC                     characterKey=40
-KEY_IDX_RED                     characterKey=31
-KEY_IDX_REWIND                  characterKey=37
-KEY_IDX_SIMPLELINK_DISC_MENU    characterKey=1
-KEY_IDX_SIMPLELINK_GUIDE        characterKey=4
-KEY_IDX_SIMPLELINK_HOME         characterKey=0
-KEY_IDX_SIMPLELINK_INFO_MENU    characterKey=2
-KEY_IDX_SIMPLELINK_POWER_ONOFF  characterKey=6
-KEY_IDX_SIMPLELINK_RECLIST      characterKey=5
-KEY_IDX_SIMPLELINK_TITLE_POPUP  characterKey=3
-KEY_IDX_SKIP_BACKWARD           characterKey=39
-KEY_IDX_SKIP_FORWARD            characterKey=38
-KEY_IDX_STB_BACK                characterKey=30
-KEY_IDX_STB_BLUE                characterKey=16
-KEY_IDX_STB_CH_DOWN             characterKey=38
-KEY_IDX_STB_CH_UP               characterKey=37
-KEY_IDX_STB_DASH                characterKey=19
-KEY_IDX_STB_DOWN                characterKey=34
-KEY_IDX_STB_EXIT                characterKey=7
-KEY_IDX_STB_FORWARD             characterKey=12
-KEY_IDX_STB_GREEN               characterKey=14
-KEY_IDX_STB_GUIDE               characterKey=2
-KEY_IDX_STB_LEFT                characterKey=35
-KEY_IDX_STB_MENU                characterKey=1
-KEY_IDX_STB_NUM0                characterKey=29
-KEY_IDX_STB_NUM1                characterKey=20
-KEY_IDX_STB_NUM2                characterKey=21
-KEY_IDX_STB_NUM3                characterKey=22
-KEY_IDX_STB_NUM4                characterKey=23
-KEY_IDX_STB_NUM5                characterKey=24
-KEY_IDX_STB_NUM6                characterKey=25
-KEY_IDX_STB_NUM7                characterKey=26
-KEY_IDX_STB_NUM8                characterKey=27
-KEY_IDX_STB_NUM9                characterKey=28
-KEY_IDX_STB_OK                  characterKey=18
-KEY_IDX_STB_PAUSE               characterKey=11
-KEY_IDX_STB_PLAY                characterKey=10
-KEY_IDX_STB_POWER_ONOFF         characterKey=0
-KEY_IDX_STB_REC                 characterKey=17
-KEY_IDX_STB_RED                 characterKey=13
-KEY_IDX_STB_REWIND              characterKey=8
-KEY_IDX_STB_RIGHT               characterKey=36
-KEY_IDX_STB_SKIP_BACKWARD       characterKey=61
-KEY_IDX_STB_SKIP_FORWARD        characterKey=62
-KEY_IDX_STB_STOP                characterKey=9
-KEY_IDX_STB_UP                  characterKey=33
-KEY_IDX_STB_YELLOW              characterKey=15
-KEY_IDX_STOP                    characterKey=35
-KEY_IDX_VOL_DOWN                characterKey=25
-KEY_IDX_VOL_UP                  characterKey=24
-KEY_IDX_YELLOW                  characterKey=32
-*/
 
 module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj, changedKeysArr, callback ) {
 
